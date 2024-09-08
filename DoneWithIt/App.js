16e79973f26a64f48ddc1f1b7646f51a9a44df21
lukethorvilson <luke.thorvilson@wsu.edu>
2024-09-07 17:00:43 -0700

    Learned the touchable without feedback component that allows for no feedback when a touchable is pressesd.

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,15 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello world!</Text>
-      {/*Load static images from files*/}
-      <Image source={{
-        width: 200,
-        height: 300,
-        uri: "https://picsum.photos/200/300"}}/>
+      <TouchableWithoutFeedback onPress={() => console.log("Image tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableWithoutFeedback>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -18,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
